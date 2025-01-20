45796d9cedf7f5c967cb34f6f69ef752f1447182
Mati Ullah <matiullah.pro@gmail.com>
2025-01-21 00:45:50 +0500

    common replaced with dev in morgan

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const printJobRoutes = require("./routes/printjobRoutes.js");
 
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(morgan("dev"));
+app.use(morgan("common"));
 // app.use(express.json());
 // Apply `express.json()` to all routes except `/api/printjob/stripe-webhook`
 app.use(
