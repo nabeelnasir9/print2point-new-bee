5ccfa7b1dd4bf2a0fc098a78aaeda674e8ef5889
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2025-03-29 01:31:07 +0500

    Update mailOrder.js

--- src/utils/mailOrder.js
+++ src/utils/mailOrder.js
@@ -40,7 +40,7 @@ const sendCustomerConfirmationEmail = async (
         margin: 0 auto;
         padding: 45px 30px 60px;
         background: #f4f7ff;
-        background-image: url(https://i.ibb.co/ckFvRHt/068a688b-2d08-4df0-9837-62c2ff3065b4.jpg);
+        background-image: url(https://i.ibb.co/5XVbj9rn/abstract-textured-backgound.jpg);
         background-repeat: no-repeat;
         background-size: 800px 452px;
         background-position: top center;
@@ -55,7 +55,7 @@ const sendCustomerConfirmationEmail = async (
               <td>
                 <img
                   alt=""
-                  src="https://i.ibb.co/NnB072R/logo-removebg-preview.png"
+                  src="https://i.ibb.co/DHg0qFG5/logo-2ecde31e1f98254db30f3f295b08703e.png"
                   height="70px"
                 />
               </td>
